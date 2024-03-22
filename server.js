606d86652ed9610e9179e4c3407e89bb40e4bb5f
Sandra Rodgers <sandra.rodgers@deepgram.com>
2024-03-22 10:21:51 -0500

    TTS basic functionality

--- server.js
+++ server.js
@@ -1,19 +1,91 @@
+const { createClient } = require("@deepgram/sdk");
 const express = require("express");
 const http = require("http");
-const { createClient } = require("@deepgram/sdk");
+const fs = require("fs");
 const dotenv = require("dotenv");
+const path = require("path");
+
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
+app.use(express.json());
 
-const deepgramClient = createClient(process.env.DEEPGRAM_API_KEY);
+const deepgram = createClient(process.env.DEEPGRAM_API_KEY);
 
 app.use(express.static("public/"));
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+app.use("/audio", express.static("audio"));
+
+app.post("/api", async (req, res) => {
+  const { body } = req;
+  const { text, model } = body;
+
+  try {
+    const filePath = await getAudio(text, model);
+    res.json({ audioUrl: filePath });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
-server.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+const getAudio = async (text, model) => {
+  const response = await deepgram.speak.request({ text }, { model });
+  const stream = await response.getStream();
+
+  if (stream) {
+    const buffer = await getAudioBuffer(stream);
+
+    try {
+      await new Promise((resolve, reject) => {
+        fs.writeFile("audio/audio.wav", buffer, (err) => {
+          if (err) {
+            console.error("Error writing audio to file:", err);
+            reject(err);
+          } else {
+            console.log("Audio file written to audio.wav");
+            resolve();
+          }
+        });
+      });
+    } catch (err) {
+      throw err;
+    }
+
+    return "/audio/audio.wav";
+  } else {
+    console.error("Error generating audio:", stream);
+    throw new Error("Error generating audio: Stream is empty");
+  }
+};
+
+// Helper function to convert stream to audio buffer
+const getAudioBuffer = async (response) => {
+  const reader = response.getReader();
+  const chunks = [];
+
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) break;
+
+    chunks.push(value);
+  }
+
+  const dataArray = chunks.reduce(
+    (acc, chunk) => Uint8Array.from([...acc, ...chunk]),
+    new Uint8Array(0)
+  );
+
+  return Buffer.from(dataArray.buffer);
+};
+
+// Serve the index.html file on root path
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
